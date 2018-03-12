57abd1b4e600c1e794cfa7af049bf209cfcd5411
binbin <729599457@qq.com>
2018-03-12 23:38:24 +0800

    * 层次优先遍历

--- java/TreeNode.java
+++ java/TreeNode.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 public class TreeNode {
 
     int value;
@@ -10,4 +12,26 @@ public class TreeNode {
     TreeNode right = null;
 }
 
+class BinaryTreeErgodic {
+
+    /**
+     * 层次优先遍历
+     */
+    public static void lavelOrderTraverse(TreeNode root) {
+        if (root == null)
+            return;
+
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        TreeNode current = null;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            current = queue.poll();
+            System.out.print(current.value + " ");
+            if (current.left != null)
+                queue.offer(current.left);
+            if (current.right != null)
+                queue.offer(current.right);
+        }
+    }
+}
 
