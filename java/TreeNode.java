1b549b3a84d58607279b2b45f7318d1f46fc63f7
binbin <729599457@qq.com>
2018-03-14 23:26:52 +0800

    检查是否是二叉查找树

--- java/TreeNode.java
+++ java/TreeNode.java
@@ -84,5 +84,24 @@ class BinaryTreeErgodic {
     }
 
 
+    public class BSTChecker {
+        private  int lastVisit = Integer.MIN_VALUE;
+
+        public  boolean isBST(Node root) {
+            if(root == null) return true;
+
+            boolean judgeLeft = isBST(root.left); // 先判断左子树
+
+            if(root.data >= lastVisit && judgeLeft) { // 当前节点比上次访问的数值要大
+                lastVisit = root.data;
+            } else {
+                return false;
+            }
+
+            boolean judgeRight = isBST(root.right); // 后判断右子树
+
+            return judgeRight;
+        }
+    }
 }
 
