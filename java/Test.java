fdb5bd35bd3d93ee95a5a26a1989082cd97beb00
binbin <729599457@qq.com>
2018-03-19 21:07:37 +0800

    正则清洗文件内容

--- java/Test.java
+++ java/Test.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Test {
 
@@ -14,8 +15,16 @@ public class Test {
     }
 
 
-    public void judgeMethod(List<Map> list){
+    public void judgeMethod(List<String> list){
 
+        String plane="";
+        for(int i=0;i<list.size();i++){
+            if(i==0){
+                //消息由字母和数字组成
+                String pattern="^[A-Za-z0-9]+$";
+
+            }
+        }
     }
 
     public static List<String> readTxtFileIntoStringArrList(String filePath){
@@ -33,7 +42,12 @@ public class Test {
             String lineText=null;
 
             while ((lineText=bufferedReader.readLine())!=null){
-                list.add(lineText);
+                String pattern = "plane.+?";
+                boolean isMatch = Pattern.matches(pattern, lineText);
+                if(isMatch){
+                    lineText.replace("}","");
+                    list.add(lineText);
+                }
             }
             bufferedReader.close();
             reader.close();
